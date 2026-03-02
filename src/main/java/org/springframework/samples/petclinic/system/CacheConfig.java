a6a4120f5755cc612041d035b5e3e1dd0c009a60
bingli-borland <1042996632@qq.com>
2026-03-02 14:13:46 +0800

    1.2.x

--- src/main/java/org/springframework/samples/petclinic/system/CacheConfig.java
+++ src/main/java/org/springframework/samples/petclinic/system/CacheConfig.java
@@ -3,9 +3,7 @@ package org.springframework.samples.petclinic.system;
 import javax.cache.configuration.Configuration;
 import javax.cache.configuration.MutableConfiguration;
 
-import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 
 /**
@@ -16,13 +14,13 @@ import org.springframework.context.annotation.Profile;
 @Profile("production")
 class CacheConfig {
 
-    @Bean
-    public JCacheManagerCustomizer cacheManagerCustomizer() {
-        return cm -> {
-            Configuration<Object, Object> cacheConfiguration = createCacheConfiguration();
-            cm.createCache("vets", cacheConfiguration);
-        };
-    }
+    // @Bean
+    // public JCacheManagerCustomizer cacheManagerCustomizer() {
+    //     return cm -> {
+    //         Configuration<Object, Object> cacheConfiguration = createCacheConfiguration();
+    //         cm.createCache("vets", cacheConfiguration);
+    //     };
+    // }
 
     private Configuration<Object, Object> createCacheConfiguration() {
         // Create a cache using infinite heap. A real application will want to use an
